cde09f5accdcba10299d87be1d62ce98d6fd1780
Ppl116 <94757078+Ppl116@users.noreply.github.com>
2021-11-21 02:07:02 +0530

    Add files via upload

---
+++ Home page.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page Layout</title>
+  <style>
+    .head1 {
+      font-size:40px;
+      color:blue;
+      font-weight:bold;
+                        text-align:center;
+    }
+    .head2 {
+      font-size:17px;
+      margin-left:10px;
+      margin-bottom:15px;
+                        text-align:center;
+    }
+    body {
+      margin: auto;
+      background-position:center;
+      background-size: contain;
+      ;
+    }
+  
+    .menu {
+      type:absolute;
+      position: sticky;
+      top: 10px;
+      background-color:blue;
+      padding:10px 0px 10px 0px;
+      color:white;
+      margin: auto;
+      overflow: hidden;
+    }
+    .menu a {
+      float: left;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 20px;
+    }
+    .menu-log {
+      right: auto;
+      float: right;
+    }
+    footer {
+      width: 100%;
+      bottom: 0px;
+      background-color:blue;
+      color:white;
+      position: absolute;
+      padding-top:20px;
+      padding-bottom:50px;
+      text-align:center;
+      font-size:20px;
+      font-weight:bold;
+    }
+    .body_sec {
+      margin-left:20px;
+    }
+    
+  </style>
+</head>
+
+<body>
+     <!-- Header Section -->
+  <header>
+    <div class="head1">PORTFOLIIO</div>
+  </header>
+  
+  <!-- Menu Navigation Bar -->
+  <div class="menu">
+      <nav>
+    
+    <a href=file:///C:/Users/SNEHA%20SINHA/Desktop/HTML/Home%20page.html>HOME</a>
+           
+    <a href=file:///C:/Users/SNEHA%20SINHA/Desktop/HTML/home.html>ABOUT</a>
+        
+        <a href=file:///C:/Users/SNEHA%20SINHA/Desktop/HTML/contact.html>CONTACT</a>
+        <a href=file:///C:/Users/SNEHA%20SINHA/Desktop/HTML/achievment.html>ACHIVEMENT</a>
+           
+    </nav>
+               
+     <div class="menu-log">
+       
+        <a href=file:///C:/Users/SNEHA%20SINHA/Desktop/HTML/profile.html>PROFILE</a>            
+    </div>
+ </div>
+         <center><h2 style="font-family: Verdana, Geneva, Tahoma, sans-serif;"><b><i>PRERANA PRIYADARSINI LAHAJAL</i></b></h2>
+        <h3>GIET UNIVERSITY,GUNUPUR</h3></center>
+        <center><img src="ppl2.jpeg" alt="Profile image" width="300px" height="500px"></center>
+ </body>
+</html>
